366be589296d963241256d8ff5706dcb307344af
Evans Musomi <evans.musomi@gmail.com>
2018-07-31 14:02:55 +0300

    feat: sets state internally and from child component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [{ name: "Evans", age: 28 }, { name: "Musomi", age: 26 }]
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [{ name: "Evanso", age: 29 }, { name: "Musoms", age: 25 }]
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name="Evans" age="28" />
-        <Person name="Musomi" age="26">
-          Hobbies: Anime
-        </Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
       </div>
     );
   }
