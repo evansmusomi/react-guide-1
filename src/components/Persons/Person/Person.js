545f16b4b83d4f387773086f9cc635d705fae872
Evans Musomi <evans.musomi@gmail.com>
2018-08-06 04:30:56 +0300

    chore: adds prop type checking in person component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+
 import classes from "./Person.css";
 import WithClass from "../../../hoc/WithClass";
 
@@ -33,5 +35,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  changed: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number
+};
 
 export default Person;
